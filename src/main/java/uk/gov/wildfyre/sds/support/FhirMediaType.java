373a7e80c6c98d0165fd513b7b845366a8f9aa01
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-09-17 09:59:04 +0100

    HAPI 4.0.0 changes

---
+++ src/main/java/uk/gov/wildfyre/sds/support/FhirMediaType.java
@@ -0,0 +1,10 @@
+package uk.gov.wildfyre.sds.support;
+
+public final class FhirMediaType {
+
+
+    public static final String APPLICATION_FHIR_JSON_VALUE = "application/fhir+json";
+
+    public static final String APPLICATION_FHIR_XML_VALUE = "application/fhir+xml";
+
+}
